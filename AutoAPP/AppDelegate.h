481876fc2cac32d164b4e88a87ebe76007dc2d98
国栋 <guodong@guodongdeMacBook-Air.local>
2015-11-25 18:25:20 +0800

    Initial Commit

---
+++ AutoAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoAPP
+//
+//  Created by 国栋 on 15/11/25.
+//  Copyright (c) 2015年 GD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoAPP
+//
+//  Created by 国栋 on 15/11/25.
+//  Copyright (c) 2015年 GD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
